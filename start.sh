904a17cad6e2fe279474f8fe75f6ed86bf2c9be6
russelg <isaac.drew@icloud.com>
2020-05-09 13:45:25 +0800

    Build docker image on demand
    Signed-off-by: russelg <isaac.drew@icloud.com>

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+docker build -t wireguard-socks-proxy .
 
 docker run \
     -d -it \
+    --name=vpn-client \
     --cap-add=NET_ADMIN \
     --publish 127.0.0.1:1080:1080 \
     --volume "$(realpath "$1"):/etc/wireguard/:ro" \
